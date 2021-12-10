2d23a2f4a5aa985e53dfe1289d935b2af95adc2d
Dmitry <dima.fenix@list.ru>
2021-12-10 20:11:19 +0300

    Add  ReviewService and RoomService in Application

---
+++ iTechArt_Bookingю.Application/Services/ReviewService.cs
@@ -0,0 +1,41 @@
+﻿using iTechArt_Booking.Domain.Interfaces;
+using iTechArt_Booking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt_Booking.Application.Services
+{
+    class ReviewService : IReviewRepository
+    {
+        public readonly IReviewRepository reviewRepository;
+
+        public ReviewService (IReviewRepository _reviewRepository)
+        {
+            reviewRepository = _reviewRepository ?? throw new ArgumentNullException(nameof(reviewRepository));
+        }
+
+
+        public void Create(Review review)
+        {
+            reviewRepository.Create(review);
+        }
+
+        public Review Delete(Guid id)
+        {
+           return reviewRepository.Delete(id);
+        }
+
+        public Review Get(Guid id)
+        {
+            return reviewRepository.Get(id);
+        }
+
+        public IEnumerable<Review> GetAll()
+        {
+            return reviewRepository.GetAll();
+        }
+    }
+}
---
+++ iTechArt_Bookingю.Application/Services/RoomService.cs
@@ -0,0 +1,30 @@
+﻿using iTechArt_Booking.Domain.Interfaces;
+using iTechArt_Booking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt_Booking.Application.Services
+{
+    class RoomService : IRoomRepository
+    {
+        public readonly IRoomRepository roomRepository;
+
+        public RoomService(IRoomRepository _roomRepository)
+        {
+            roomRepository = _roomRepository ?? throw new ArgumentNullException(nameof(roomRepository));
+        }
+
+        public void Create(Room room)
+        {
+            roomRepository.Create(room);
+        }
+
+        public IEnumerable<Room> GetAll()
+        {
+            return roomRepository.GetAll();
+        }
+    }
+}
