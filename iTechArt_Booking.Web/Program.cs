ebfbe0310cd7b11eb50fa9ef9b0bc627b3eb98fe
Dmitry <dima.fenix@list.ru>
2022-04-06 23:25:55 +0300

    Addes class for init admin role in db. Infastructure->Repositoriesd->IntitDataBase.InitializatioanData.cs

--- iTechArt_Booking.Web/Program.cs
+++ iTechArt_Booking.Web/Program.cs
@@ -1,5 +1,7 @@
+using iTechArt_Booking.Infastructure.Repositories.EFRepository;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,25 @@ namespace iTechArt_Booking.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<BookingDBContext>();
+                  //  SampleData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
